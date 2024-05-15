dae51acbaa78347c2fe0cf54a9f3bb7f51714ab7
Ershi-Gu <2402554312@qq.com>
2024-05-16 02:14:27 +0800

    新增登录注册功能，优化了一些页面小 bug

---
+++ src/pages/User/Register/index.tsx
@@ -0,0 +1,217 @@
+import { Footer } from '@/components';
+import { userRegisterUsingPost } from '@/services/ershi-bi/userController';
+import { LockOutlined, UserOutlined } from '@ant-design/icons';
+import { LoginForm, ProFormText } from '@ant-design/pro-components';
+import { Helmet, Link, history } from '@umijs/max';
+import { Alert, Button, Tabs, message } from 'antd';
+import { createStyles } from 'antd-style';
+import React, { useState } from 'react';
+import Settings from '../../../../config/defaultSettings';
+
+const useStyles = createStyles(({ token }) => {
+  return {
+    action: {
+      marginLeft: '8px',
+      color: 'rgba(0, 0, 0, 0.2)',
+      fontSize: '24px',
+      verticalAlign: 'middle',
+      cursor: 'pointer',
+      transition: 'color 0.3s',
+      '&:hover': {
+        color: token.colorPrimaryActive,
+      },
+    },
+    lang: {
+      width: 42,
+      height: 42,
+      lineHeight: '42px',
+      position: 'fixed',
+      right: 16,
+      borderRadius: token.borderRadius,
+      ':hover': {
+        backgroundColor: token.colorBgTextHover,
+      },
+    },
+    container: {
+      display: 'flex',
+      flexDirection: 'column',
+      height: '100vh',
+      overflow: 'auto',
+      backgroundImage:
+        "url('https://mdn.alipayobjects.com/yuyan_qk0oxh/afts/img/V-_oS6r-i7wAAAAAAAAAAAAAFl94AQBr')",
+      backgroundSize: '100% 100%',
+    },
+  };
+});
+const Lang = () => {
+  return;
+};
+const RegisterMessage: React.FC<{
+  content: string;
+}> = ({ content }) => {
+  return (
+    <Alert
+      style={{
+        marginBottom: 24,
+      }}
+      message={content}
+      type="error"
+      showIcon
+    />
+  );
+};
+const Register: React.FC = () => {
+  const [userRegisterState, setUserRegisterState] = useState<API.RegisterResult>({});
+  const [type, setType] = useState<string>('account');
+  const { styles } = useStyles();
+
+  // 表单提交
+  const handleSubmit = async (values: API.UserRegisterRequest) => {
+    const { userPassword, checkPassword } = values;
+
+    // 提交数据前端校验
+    if (userPassword !== checkPassword) {
+      message.error('两次输入密码不一致');
+      return;
+    }
+    try {
+      // 注册
+      const res = await userRegisterUsingPost({
+        ...values,
+      });
+      if (res.data > 0) {
+        const defaultRegisterSuccessMessage = '注册成功！';
+        message.success(defaultRegisterSuccessMessage);
+        // 重定向跳转至原页面
+        const urlParams = new URL(window.location.href).searchParams;
+        // history.push(urlParams.get('redirect') || '/');
+        history.push('/user/login?redirect=' + urlParams);
+        return;
+      }
+      // console.log(msg);
+      // 如果失败去设置用户错误信息
+      // setUserRegisterState(msg);
+      throw new Error(`register error id = ${id}`);
+    } catch (error) {
+      const defaultRegisterFailureMessage = '注册失败，请重试！';
+      console.log(error);
+      message.error(defaultRegisterFailureMessage);
+    }
+  };
+  const { status, type: loginType } = userRegisterState;
+  return (
+    <div className={styles.container}>
+      <Helmet>
+        <title>
+          {'注册'}- {Settings.title}
+        </title>
+      </Helmet>
+      <Lang />
+      <div
+        style={{
+          flex: '1',
+          padding: '32px 0',
+        }}
+      >
+        <LoginForm
+          // 修改注册按钮
+          submitter={{
+            searchConfig: {
+              submitText: '注冊',
+            },
+          }}
+          contentStyle={{
+            minWidth: 280,
+            maxWidth: '75vw',
+          }}
+          logo={<img alt="logo" src="/logo.svg" />}
+          title="云智能 BI"
+          subTitle={'云智能 BI 是一款好用的智能 BI 数据分析平台'}
+          // initialValues={{
+          //   autoRegister: true,
+          // }}
+          onFinish={async (values) => {
+            await handleSubmit(values as API.RegisterParams);
+          }}
+        >
+          <Tabs
+            activeKey={type}
+            onChange={setType}
+            centered
+            items={[
+              {
+                key: 'account',
+                label: '账号密码注册',
+              },
+            ]}
+          />
+          {type === 'account' && (
+            <>
+              <ProFormText
+                name="userAccount"
+                fieldProps={{
+                  size: 'large',
+                  prefix: <UserOutlined />,
+                }}
+                placeholder={'请输入账号'}
+                rules={[
+                  {
+                    required: true,
+                    message: '账号是必填项！',
+                  },
+                  {
+                    min: 6, // 账号少于6位,
+                    message: '账号不能少于 6 位',
+                  },
+                ]}
+              />
+              <ProFormText.Password
+                name="userPassword"
+                fieldProps={{
+                  size: 'large',
+                  prefix: <LockOutlined />,
+                }}
+                placeholder={'请输入密码'}
+                rules={[
+                  {
+                    required: true,
+                    message: '密码是必填项！',
+                  },
+                  {
+                    min: 8, // 密码少于6位
+                    message: '密码不能少于 8 位',
+                  },
+                ]}
+              />
+              <ProFormText.Password
+                name="checkPassword"
+                fieldProps={{
+                  size: 'large',
+                  prefix: <LockOutlined />,
+                }}
+                placeholder={'请再次输入密码'}
+                rules={[
+                  {
+                    required: true,
+                    message: '请再次输入密码！',
+                  },
+                ]}
+              />
+            </>
+          )}
+          <div
+            style={{
+              marginBottom: 24,
+            }}
+          >
+            <Button block size={'small'} type={'link'}>
+              <Link to="/user/login">返回登录</Link>
+            </Button>
+          </div>
+        </LoginForm>
+      </div>
+      <Footer />
+    </div>
+  );
+};
+export default Register;
